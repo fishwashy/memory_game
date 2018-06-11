d5e3ad1f53bddebf80e1ac753f23a9de5bde2685
root <root@localhost.localdomain>
2018-06-11 18:41:03 +0000

    emory game_0.3

--- memory/game.java
+++ memory/game.java
@@ -2,18 +2,18 @@ import java.util.Collections;
 import java.util.ArrayList;
 
 public class game{
-	public void game(String[] String_Array,int input) {
+	public static void game(String[] String_Array,int input) {
 
-		private ArrayList numbers = new ArrayList();
-		private int k = 0;
+		ArrayList numbers = new ArrayList();
+		int k = 0;
 
-		for(int i = 0; i < 10; i++){
-			numbers.add(i+1);
+		for(int i = 0; i < input; i++){
+			numbers.add(i);
 		}
 
 		Collections.shuffle(numbers);
 
-		for(int i = 0;i < 10;i++){
+		for(int i = 0;i < input;i++){
 			try{
 				k = (Integer) numbers.get(i);
 				System.out.println(String_Array[k]);
